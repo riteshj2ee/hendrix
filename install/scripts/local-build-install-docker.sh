a283a29d13a2a54285af7b9f1aae0d395d55bda8
ambud_sharma <ambud_sharma@symantec.com>
2016-07-11 16:51:19 -0700

    Fixing configuration and images for docker deployment

--- install/scripts/local-build-install-docker.sh
+++ install/scripts/local-build-install-docker.sh
@@ -17,10 +17,10 @@ export MYSQL_ROOT_PASSWORD=root
 # docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
 
 # Stop any existing revisions of the containers
-docker-compose -p hendrix -f ../conf/local/docker-compose.yml stop -t 2
+docker-compose -f ../conf/local/docker-compose-backup.yml stop -t 2
 
 # Remove any existing revisions of the containers
-docker-compose -p hendrix -f ../conf/local/docker-compose.yml rm --force
+docker-compose -f ../conf/local/docker-compose-backup.yml rm --force
 
 # docker ps -a | grep 'registry' | awk '{print $1}' | xargs docker rm -f
 
@@ -36,13 +36,14 @@ export HVERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -
 echo "HVERSION is $HVERSION"
 
 cd install/scripts
-docker-compose -p hendrix -f ../conf/local/docker-compose.yml up -d
+docker-compose -f ../conf/local/docker-compose-backup.yml up -d
 
 # Download Apache Storm to launch topologies
 # wget http://apache.cs.utah.edu/storm/apache-storm-0.10.1/apache-storm-0.10.1.tar.gz -O /tmp/storm.tar.gz
 # wget http://apache.cs.utah.edu/kafka/0.8.2.1/kafka_2.9.1-0.8.2.1.tgz -O /tmp/kafka.tar.gz
 
-cd ../target/
+mkdir -p ../target/
+cd ../target
 rm -rf storm
 rm -rf kafka
 cp /tmp/storm.tar.gz .
@@ -52,15 +53,14 @@ tar xf kafka.tar.gz
 mv apache-storm-0.10.1 storm
 mv kafka_2.9.1-0.8.2.1 kafka
 
-docker-compose -p hendrix -f ../conf/local/docker-compose.yml scale kafka=5
-docker-compose -p hendrix -f ../conf/local/docker-compose.yml scale supervisor=3
+docker-compose -f ../conf/local/docker-compose-backup.yml scale kafka=5
+docker-compose -f ../conf/local/docker-compose-backup.yml scale supervisor=4
 
 while ! nc -z localhost 49627;do echo "Checking nimbus availability";sleep 1;done
 
 while ! `./storm/bin/storm list -c nimbus.thrift.port=49627 2>&1 | grep -q "No topologies"`;do echo "Waiting for Storm Nimbus to come online";sleep 1s; done || echo "Now deploying topology"
 
 kafka/bin/kafka-topics.sh --describe --zookeeper localhost:2181 
-
 kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic ruleTopic --replication-factor 3 --partitions 1
 kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic templateTopic --replication-factor 3 --partitions 1
 
